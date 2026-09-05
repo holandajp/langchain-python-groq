b3e430cb25893e85c9f18bf13786ea26ff92a8d6
unknown <joaocmdt505@gmail.com>
2026-09-05 11:18:47 -0300

    feat: adiciona resposta do modelo como um objeto json

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.tracers.stdout import ConsoleCallbackHandler
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
+
+set_debug(True)
 
 load_dotenv()
 api_key = os.getenv("GROQ_API_KEY")
 
+class Destino(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("motivo pelo qual é interessante visitar essa cidade")
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
 prompt_cidade = PromptTemplate(
     template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,    
-    input_variables=["interesse"]
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
 )
 
 modelo = ChatGroq(
@@ -20,11 +33,14 @@ modelo = ChatGroq(
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia = prompt_cidade | modelo | parseador
 
 resposta = cadeia.invoke(
     {
         "interesse" : "praias"
+    },
+    config={
+        "callbacks": [ConsoleCallbackHandler()]
     }
 )
 print(resposta)
