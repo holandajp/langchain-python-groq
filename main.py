6a5937869edf3f6ed86049b411b7d3f76298eb47
unknown <joaocmdt505@gmail.com>
2026-09-05 17:10:11 -0300

    feat: adiciona uma sequencia de cadeias com lcel

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ class Destino(BaseModel):
     cidade:str = Field("A cidade recomendada para visitar")
     motivo:str = Field("motivo pelo qual é interessante visitar essa cidade")
 
-parseador = JsonOutputParser(pydantic_object=Destino)
+class Restaurantes(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    restaurantes:str = Field("Restaurantes recomendados na cidade")
+
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restaurantes)
 
 prompt_cidade = PromptTemplate(
     template="""
@@ -24,7 +29,19 @@ prompt_cidade = PromptTemplate(
     {formato_de_saida}
     """,    
     input_variables=["interesse"],
-    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
+    partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()}
+)
+
+prompt_restaurantes = PromptTemplate(
+    template="""
+    Sugira restaurantes populares entre locais em {cidade}.
+    {formato_de_saida}
+    """,    
+    partial_variables={"formato_de_saida": parseador_restaurantes.get_format_instructions()}
+)
+
+prompt_cultura = PromptTemplate(
+    template="Sugira atividades e locais culturais em {cidade}."
 )
 
 modelo = ChatGroq(
@@ -33,7 +50,11 @@ modelo = ChatGroq(
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | parseador
+cadeia_1 = prompt_cidade | modelo | parseador_destino
+cadeia_2 = prompt_restaurantes | modelo | parseador_restaurantes
+cadeia_3 = prompt_cultura | modelo | StrOutputParser()
+
+cadeia = (cadeia_1 | cadeia_2 | cadeia_3)
 
 resposta = cadeia.invoke(
     {
